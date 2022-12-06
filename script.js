4c2dfa8a6feeb52dfb5cf540641cf2802b6c0976
Runar Saur Modahl <runarmod@gmail.com>
2022-12-06 21:41:32 +0100

    shuffle questions

--- script.js
+++ script.js
@@ -539,4 +539,26 @@ function previousQuestion() {
   reset();
 }
 
+// COPY PASTE FROM https://stackoverflow.com/a/2450976/10880273 !
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
+questions = shuffle(questions)
 displayQuestion();
